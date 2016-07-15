ec48f55b66b213bfb1ae5df39dd39839aa68066d
Liye He <heliye89@gmail.com>
2016-07-15 14:18:28 +0300

    adding Emin and Emax parameters for baseline correction

--- R/Correct_plate.R
+++ R/Correct_plate.R
@@ -2,21 +2,25 @@
 #'
 #' A function to do baseline correction on the dose-response matrix for drug combinations with a weighted correction fator
 #'
-#' @param response.mat a dose-response matrix with concentrations as row names and column names
-#' @return A list of the original dose-response matrix without correction and the corrected dose-response matrix
+#' @param response.mat a dose-response matrix with concentrations as row names and column names.
+#' @param Emin the minimal effect of the drug used in the 4-parameter log-logistic function to fit the dose-response 
+#' curve. If it is not NA, it is fixed the value assigned by the user. Defaults to NA. 
+#' @param Emax the maximal effect of the drug used in the 4-parameter log-logistic function to fit the dose-response 
+#' curve. If it is not NA, it is fixed the value assigned by the user. Defaults to NA. 
+#' @return A list of the original dose-response matrix without correction and the corrected dose-response matrix.
 #' @author Liye He \email{liye.he@helsinki.fi}, Jing Tang \email{jing.tang@helsinki.fi}
 #' @examples
 #' data("mathews_screening_data")
 #' data <- ReshapeData(mathews_screening_data)
 #' data <- BaselineCorrectionSD(data$dose.response.mats[[1]])
-BaselineCorrectionSD <- function (response.mat) {
+BaselineCorrectionSD <- function (response.mat, Emin = NA, Emax = NA) {
   pm <- response.mat
   # check if response.mat has row names and column names
   if (is.null(rownames(response.mat)) | is.null(colnames(response.mat))) {
     stop("Please provide drug contrations as row names and column names!")
   }
 
-  single.fitted <- FittingSingleDrug(response.mat)
+  single.fitted <- FittingSingleDrug(response.mat, c(NA, Emin, Emax, NA))
 
   baseline <- (min(as.numeric(single.fitted$drug.row.fitted)) +
                  min(as.numeric(single.fitted$drug.col.fitted)))/2
